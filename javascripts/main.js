4f4be795750dcf0545f2d2d7df01f79d8fbfcc79
Satanwoo <applewuziqi@gmail.com>
2014-05-05 02:00:59 +0800

    Ziqi:Temp Logic

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,53 +1,52 @@
-var sectionHeight = function() {
-  var total    = $(window).height(),
-      $section = $('section').css('height','auto');
-
-  if ($section.outerHeight(true) < total) {
-    var margin = $section.outerHeight(true) - $section.height();
-    $section.height(total - margin - 20);
-  } else {
-    $section.css('height','auto');
-  }
-}
-
-$(window).resize(sectionHeight);
-
+/* Jquery */
 $(document).ready(function(){
-  $("section h1, section h2").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    $("nav ul li:first-child a").parent().addClass("active");
-  });
-  
-  $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
-    $(this).parent().addClass("active");
-    event.preventDefault();    
-  });
-  
-  sectionHeight();
-  
-  $('img').load(sectionHeight);
+
+    var operand = 0.0;
+    var clickOperator = '';
+    var dotClicked = false;
+    $(document).on('click', '#calculator span', function () { 
+      var input = $('.result');
+      var inputVal = input.html();
+      var htmlString = $(this).html();
+
+      if ($(this).hasClass('remove')) {
+        // Clear 
+        input.html('');
+        operand = 0.0;
+        clickOperator = '';
+
+      } else if ($(this).hasClass('operator')) {
+        operand = parseFloat(inputVal);
+        
+        input.html('');
+
+        if (htmlString == '+') {  
+          clickOperator = '+';
+        } else if (htmlString == '-') {
+          clickOperator = '-';
+        } else if (htmlString == '÷') {
+          clickOperator = '÷';
+        } else if (htmlString == 'x') {
+          clickOperator = 'x';
+        }
+
+      } else if ($(this).hasClass('equal')) {
+        var rightOperand = parseFloat(inputVal);
+        var result = 0;
+        if (clickOperator == '+') {  
+          result = operand + rightOperand;
+        } else if (clickOperator == '-') {
+          result = operand - rightOperand;
+        } else if (clickOperator == '÷') {
+          result = operand / rightOperand;
+        } else if (clickOperator == 'x') {
+          result = operand * rightOperand;
+        }
+        dotClicked = false;
+        input.html(result.toString());
+      } else {
+        input.html(inputVal + htmlString);
+      }
+
+    });
 });
-
-fixScale = function(doc) {
-
-  var addEvent = 'addEventListener',
-      type = 'gesturestart',
-      qsa = 'querySelectorAll',
-      scales = [1, 1],
-      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
-
-  function fix() {
-    meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
-    doc.removeEventListener(type, fix, true);
-  }
-
-  if ((meta = meta[meta.length - 1]) && addEvent in doc) {
-    fix();
-    scales = [.25, 1.6];
-    doc[addEvent](type, fix, true);
-  }
-};
